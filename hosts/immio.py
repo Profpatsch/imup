7e939439121ea472c47ce2e65ed7afd04592b38d
Profpatsch <mail@profpatsch.de>
2013-03-13 16:47:47 +0100

    Immio host finished (untested).

--- hosts/immio.py
+++ hosts/immio.py
@@ -1,13 +1,18 @@
-import imagehost
+from hosts.imagehost import *
 import json
-from ..libposter.encode import multipart_encode
-from ..libposter.streaminghttp import register_openers
-import urllib2
+from libposter.encode import multipart_encode
+import urllib
+
+POST_URL = "http://imm.io/store/"
 
 class Immio(Imagehost):
-    def get_link(self, filename):
-        self.fn = filename
-        if self.file_is_image()
+    """imm.io"""
+    def __init__(self):
+        super().__init__();
+        self.POST_URL = POST_URL
 
-
-
+    def _handle_server_answer(self, answer):
+        jansw = json.loads(answer)
+        if !jansw["success"]:
+            return ImagehostError(json.dumps(janw["payload"]))
+        return jansw["payload"]["uri"]
