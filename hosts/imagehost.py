978c59582b266d797d51049865390b554e008380
Profpatsch <mail@profpatsch.de>
2013-05-18 23:11:52 +0200

    Call xdg-mime as fallback now to check if it’s an image.
    Plus, check if the file exists.
    Plus, give a hint which filetype it was that didn’t work.

--- hosts/imagehost.py
+++ hosts/imagehost.py
@@ -6,8 +6,10 @@ import urllib2
 import mimetypes
 import re
 import logging as log
+import os
 
 POST_URL = None
+
 
 class Imagehost(object):
     """Abstract base class for image hosts."""
@@ -17,32 +19,51 @@ class Imagehost(object):
 
     def get_link(self, filename):
         """Upload image to this image host and return a link to it.
-        
+
         Error:
             ImagehostError
             FiletypeError (image not accepted)
             URLError (Couldn’t contact server.)
         """
         self.fn = filename
-        if not self.file_is_image():
-            raise FiletypeError("File is no image!")
+        if not os.path.exists(self.fn):
+            raise IOError("File doesn’t exist!")
+        is_image, filetype = self.file_is_image()
+        if not is_image:
+            if filetype is None:
+                raise FiletypeError("File is no image.")
+            else:
+                raise FiletypeError("File is no image, it’s Mimtype is {0}!"
+                        .format(filetype))
+
         log.info("Uploading image…")
-        answer = self._request_post();
+        answer = self._request_post()
         log.info("Image uploaded, evaluating server response…")
         link = self._handle_server_answer(answer)
         return link
 
     def file_is_image(self):
-        """Checks if file is an image MIME-type"""
+        """Checks if file is an image MIME-type.
+
+        Returns: (bool, mimestring)
+        """
         type, _ = mimetypes.guess_type(self.fn, strict=False)
-        if type:
-            if re.match(r"image/", type):
-                return True
-        return False
+        if type is None:
+            import subprocess
+            try:
+                p = subprocess.Popen(["xdg-mime", "query", "filetype", self.fn], stdout=subprocess.PIPE)
+                out, _ = p.communicate()
+                type = out.strip()
+            except OSError:
+                return (False, None)
+        if re.match(r"image/", type):
+            return (True, None)
+        else:
+            return (False, type)
 
     def _request_post(self):
         """Post the image to the URL specified in self.POST_URL
-        
+
         Returns: Server answer (str)
         Error: URLError
         """
@@ -57,12 +78,12 @@ class Imagehost(object):
         """Handles the strings returned by the server, mostly JSON or XML.
         Most likely needs to be implemented by each image host module in a
         different way.
-        
+
         Input: Server answer
         Returns: Link to image
         Error: ImagehostError
         """
         pass
 
-class FiletypeError(Exception): pass
+class FiletypeError(IOError): pass
 class ImagehostError(Exception): pass
